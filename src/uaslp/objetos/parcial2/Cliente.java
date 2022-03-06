bb0c3f71c0f53bc0f2261cb7a876173be619fd95
AngSan22 <jesuszx22@gmail.com>
2022-03-06 13:04:19 -0600

    Tarea 1-Herencia

---
+++ src/uaslp/objetos/parcial2/Cliente.java
@@ -0,0 +1,16 @@
+package uaslp.objetos.parcial2;
+
+public class Cliente extends Persona{
+    private String telefonoDeContatcto;
+
+    public void mostrar(){
+    }
+
+    public String getTelefonoDeContatcto() {
+        return telefonoDeContatcto;
+    }
+
+    public void setTelefonoDeContatcto(String telefonoDeContatcto) {
+        this.telefonoDeContatcto = telefonoDeContatcto;
+    }
+}
---
+++ src/uaslp/objetos/parcial2/Directivo.java
@@ -0,0 +1,16 @@
+package uaslp.objetos.parcial2;
+
+public class Directivo extends Empleado{
+    String categoria;
+
+    public void mostrar(){
+    }
+
+    public void setCategoria(String categoria){
+        this.categoria = categoria;
+    }
+
+    public String getCategoria(){
+        return categoria;
+    }
+}
